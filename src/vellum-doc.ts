eee8b59046facf3cff57297550055ebe9c925fbb
R.G. Wood <ric@grislyeye.com>
2024-06-13 15:03:12 +0100

    Add: support for styling index background

--- src/vellum-doc.ts
+++ src/vellum-doc.ts
@@ -20,6 +20,7 @@ export class VellumDocument extends LitElement {
       padding-right: calc(var(--gap, 0px) / 2);
       min-height: 100vh;
       color: var(--index-color, 'inherit');
+      background: var(--index-background, 'inherit');
 
       --font-family: var(--index-font-family, san-serif);
 
