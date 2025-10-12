7119f045833344bb76f21a9b9bfde9327933786b
R.G. Wood <ric@grislyeye.com>
2025-10-12 13:19:33 +0100

    Chore: tidy linting configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,20 @@
+import { defineConfig } from 'eslint/config'
 import eslint from '@eslint/js'
 import tseslint from 'typescript-eslint'
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
-export default tseslint.config(
+export default defineConfig(
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
   eslintPluginPrettierRecommended,
   {
+    ignores: [
+      'dist/**/*.ts',
+      'dist/**',
+      '**/*.mjs',
+      'eslint.config.mjs',
+      '**/*.js'
+    ],
     languageOptions: {
       parserOptions: {
         projectService: true,
